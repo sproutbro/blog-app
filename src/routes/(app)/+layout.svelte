0fa7b98cf3a0a016a2bc05af7b14b0955ed91abe
Jackie <krsproutbro@google.com>
2024-12-22 16:25:45 +0900

    navbar 추가

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,46 @@
+<script>
+    let isMenuOpen = false;
+
+    const toggleMenu = () => {
+        isMenuOpen = !isMenuOpen;
+    };
+
+    const navLinks = [
+        { href: "/", label: "Home" },
+        { href: "/profile", label: "Profile" },
+        { href: "/daily", label: "Daily" },
+        { href: "/develop", label: "Develop" },
+        { href: "/board", label: "Board" },
+    ];
+</script>
+
+<header>
+    <h1>Blog</h1>
+    <button class="menu-button" on:click={toggleMenu}> &#9776; </button>
+    <nav>
+        {#each navLinks as { href, label }}
+            <a {href}>{label}</a>
+        {/each}
+        <a href="/auth/login" class="login-btn">Login</a>
+    </nav>
+    <div class="mobile-menu {isMenuOpen ? 'open' : ''}">
+        {#each navLinks as { href, label }}
+            <a {href} on:click={() => (isMenuOpen = false)}>{label}</a>
+        {/each}
+        <a
+            href="/auth/login"
+            class="login-btn"
+            on:click={() => (isMenuOpen = false)}>Login</a
+        >
+
+        <!-- {#if data.isLogin}
+			<a href="/user/profile" class="login-btn" on:click={() => (isMenuOpen = false)}>User</a>
+		{:else}
+			<a href="/auth/login" class="login-btn" on:click={() => (isMenuOpen = false)}>Login</a>
+		{/if} -->
+    </div>
+</header>
+
+<main>
+    <slot />
+</main>
